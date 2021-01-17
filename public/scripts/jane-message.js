b8aecb180ecbff4b7d3b7f31de7f0580dc9089dd
Chris <cng177@my.bcit.ca>
2021-01-16 23:12:16 -0800

    Get messages from API

---
+++ public/scripts/jane-message.js
@@ -0,0 +1,9 @@
+const res = fetch('/api/messages').then(res => res.json()).then(data => {
+  const messages = data.messages[0].messages.split(',')
+  console.log(messages)
+
+  const messageNodes = document.querySelectorAll('.msg')
+  messageNodes.forEach((node,i) => {
+    node.textContent = messages[i].replace('^^', '')
+  })
+})
